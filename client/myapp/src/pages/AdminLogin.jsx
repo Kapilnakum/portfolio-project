b85cad2da1b7ecd1909256fcb6d082c0afb71ee1
Kapilnakum <kapilnakum1212@gmail.com>
2026-01-09 18:19:24 +0530

    Change API endpoint to local server for login

--- client/myapp/src/pages/AdminLogin.jsx
+++ client/myapp/src/pages/AdminLogin.jsx
@@ -14,7 +14,7 @@ function AdminLogin() {
     setLoading(true);
 
     try {
-      const res = await axios.post("https://portfolio-project-server-txys.onrender.com", {
+      const res = await axios.post("http://localhost:5000/api/admin/login", {
         username,
         password,
       });
