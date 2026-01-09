7f0db90e36b9a8f23f9f0b1bd0a09e9b367a7468
Nakum Kapil <kapilnakum1212@gmail.com>
2026-01-09 14:36:11 +0530

    first-commit

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const enquiryRoutes = require("./routes/enquiryRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
